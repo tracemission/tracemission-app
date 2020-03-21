2ed698f19737d20b5b9030c228dd67c78a4dd77a
Stefan Blamberg <stefan.blamberg@sap.com>
2020-03-21 17:02:17 +0100

    Start registration and landing page implementation

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Dashboard = () => {
+  return (
+    <View>
+      <Text>Just throw the component away if there is a merge conflict.</Text>
+    </View>
+  );
+};
+
+export default Dashboard;
