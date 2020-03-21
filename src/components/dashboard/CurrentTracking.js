6158e02043970d55b35f202be144274dc1e0baca
Jonathan Knöppler <j.knoeppler@gmail.com>
2020-03-21 22:19:06 +0100

    First complete mock dashboard

--- src/components/dashboard/CurrentTracking.js
+++ src/components/dashboard/CurrentTracking.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 
 const CurrentTracking = () => {
     return (
-        <Text>Now in CurrentTracking</Text>
+        <View>
+            <Card>
+                <Text style={{fontWeight: 'bold'}}>Sie befinden sich jetzt hier:</Text>
+                <Text style={{fontWeight: 'bold', fontSize: 30}}>Starbucks Cafe</Text>
+                <Text style={{fontWeight: 'bold', color: '#788896', fontSize: 30}}>1:33 min</Text>
+                <Button title='Aufenthalt Beenden'/>
+            </Card>
+        </View>
     );
 }
 
