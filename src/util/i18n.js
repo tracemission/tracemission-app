5ce88fde4d649cce30b3a58e00985b12f8b68975
Janik Stracke <j.stracke@itsmind.com>
2020-03-22 01:28:39 +0100

    Fix localization

--- src/util/i18n.js
+++ src/util/i18n.js
@@ -2,9 +2,11 @@ import i18n from 'i18n-js';
 import * as Localization from 'expo-localization';
 import translations from './locales';
 
-i18n.defaultLocale = process.env.DEFAULT_LOCALE;
-i18n.locale = Localization.locale;
-i18n.fallbacks = true;
-i18n.translations = translations;
+export const init = () => {
+    i18n.defaultLocale = process.env.DEFAULT_LOCALE;
+    i18n.locale = Localization.locale.split("-", 2)[0];
+    i18n.fallbacks = true;
+    i18n.translations = translations;
+};
 
 export default i18n;
--- src/util/locales/index.js
+++ src/util/locales/index.js
@@ -1,10 +1,10 @@
 import de from './de.i18n';
-import en_GB from './en-GB.i18n';
+import en from './en.i18n';
 
 export { default as de } from './de.i18n';
-export { default as en_GB } from './en-GB.i18n';
+export { default as en } from './en.i18n';
 
 export default {
     de,
-    'en-GB': en_GB,
+    en,
 };
